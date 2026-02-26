b6440041720226d8ce90a378a8b4f62c369db7fc
Christoffer Brandt <christobran@gmail.com>
2026-02-26 09:52:51 +0100

    Sewed together the game logic and game visuals for an OK prototype

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -1,3 +1,18 @@
+const imagePaths = [
+  "assets/images/ball.jpg",
+  "assets/images/car.jpg",
+  "assets/images/dice.jpg",
+  "assets/images/donut.jpg",
+  "assets/images/frog.jpg",
+  "assets/images/pinkheart.jpg",
+  "assets/images/pizza.jpg",
+  "assets/images/planet.jpg",
+  "assets/images/pony.jpg",
+  "assets/images/poop.jpg",
+  "assets/images/ring.jpg",
+  "assets/images/star.jpg",
+];
+
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -13,12 +28,13 @@ function checkWin() {
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const boardWidth = parseInt(urlParams.get("boardWidth"));
-const boardHeight = parseInt(urlParams.get("boardHeight"));
-const viewTime = parseInt(urlParams.get("viewTime")) * 1000; // Convert to milliseconds
+const boardWidth = parseInt(urlParams.get("boardWidth")) || 4; // Default to 4 if not provided
+const boardHeight = parseInt(urlParams.get("boardHeight")) || 4; // Default to 4 if not provided
+const viewTime = parseInt(urlParams.get("viewTime")) * 1000 || 1000; // Convert to milliseconds
 
-const gameBoard = document.getElementById("game-board");
+const gameBoard = document.getElementById("memory-container");
 gameBoard.style.gridTemplateColumns = `repeat(${boardWidth}, 1fr)`;
+gameBoard.style.gridTemplateRows = `repeat(${boardHeight}, 1fr)`;
 
 let attempts = 0;
 let recordAttempts = localStorage.getItem("recordAttempts") || Infinity;
@@ -33,17 +49,19 @@ for (let i = 0; i < (boardWidth * boardHeight) / 2; i++) {
 shuffle(cards);
 
 let firstCard = null;
+let frozen = false;
 
 // Create card elements and add click event listeners
 for (let i = 0; i < cards.length; i++) {
   const card = document.createElement("div");
   card.classList.add("card");
   card.dataset.value = cards[i];
-  card.innerHTML = `<img src="images/${cards[i]}.png" alt="Card Image">`;
+  card.innerHTML = `<img src="${imagePaths[cards[i]]}" alt="Card Image">`;
 
   card.addEventListener("click", () => {
     // Prevent clicking on already flipped cards or the same card
-    if (card.classList.contains("flipped") || firstCard === card) return;
+    if (card.classList.contains("flipped") || firstCard === card || frozen)
+      return;
     card.classList.add("flipped");
 
     // If it's the first card, store it. Otherwise, check for a match.
@@ -66,10 +84,12 @@ for (let i = 0; i < cards.length; i++) {
           }, 500);
         }
       } else {
+        frozen = true;
         setTimeout(() => {
           firstCard.classList.remove("flipped");
           card.classList.remove("flipped");
           firstCard = null;
+          frozen = false;
         }, viewTime);
       }
     }
